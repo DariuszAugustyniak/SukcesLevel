23abbbb292db41b2f8a8165f8f90b425b5af1405
DariuszAugustyniak <41304670+DariuszAugustyniak@users.noreply.github.com>
2022-03-19 11:31:47 +0100

    wyściłem nie używane kawałki (głównie inne klasy) i pamiątki z maina

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-     /*   Runda zero = new Runda();
-        zero.pierwsza();
-        zero.printAll();
-        Runda jeden = zero.iteracja(zero);
-        jeden.printAll();
-       // jeden.printChanceLagrerThan(4);
-        Runda dwa = jeden.iteracja(jeden);
-        dwa.printChanceLagrerThan(20);
-        dwa.printAll();
-        Runda trzy = dwa.iteracja(dwa);
-        trzy.printChanceLagrerThan(20);
-        trzy.printAll();
-        Runda cztery = trzy.iteracja(trzy);
 
-        cztery.printChanceLagrerThan(20);
-        cztery.printAll();/*/
-	// write your code here
 
         WarstwaSymulacji pierwsza = new WarstwaSymulacji();
         pierwsza.wypełnijPoRówno();
@@ -31,15 +15,7 @@ public class Main {
         druga.wypisz();
         druga.wyspiszSzanse();
         druga.szansaNaConajmniej(7);
-       /* WarstwaSymulacji druga = pierwsza.dodajNowąWarstwę();
-        druga.wypisz();
-        druga.podajSLWiekszeOdProcenty(6);
-        WarstwaSymulacji trzecia =druga.dodajNowąWarstwę(pierwsza);
-        trzecia.wypisz();
-        trzecia.podajSLWiekszeOdProcenty(6);
-        WarstwaSymulacji czwarta =trzecia.dodajNowąWarstwę(pierwsza);
-        czwarta.wypisz();
-        czwarta.podajSLWiekszeOdProcenty(6);*/
+
 
     }
 }
--- src/com/company/WarstwaSymulacji.java
+++ src/com/company/WarstwaSymulacji.java
@@ -1,9 +1,9 @@
 package com.company;
 
-import java.util.Collection;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+
 
 public class WarstwaSymulacji {
 
